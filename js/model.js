533c6712635119b1d481670bc405536084fd13c0
PipZip <zipperpipper@gmail.com>
2020-08-04 21:49:18 +0600

    js

--- js/model.js
+++ js/model.js
@@ -1,6 +1,35 @@
-$('img.mapster').mapster();
+var image = $('img.mapster');
 
+image.mapster();
 
+var resizing,
+    body= $(body),
+    win=$('.model_view_window'),
+    diffW=win.width() - image.width(),
+    lastw=win.innerWidth(),
+    lasth=win.innerHeight();
+
+var resize = function() {
+    var win= $('.model_view_window'),
+        width=win.width(), height=win.height();
+    // only try to resize every 200 ms
+    if (resizing) {
+        return;
+    }
+    if (lastw !== width || lasth !== height) {
+        resizing=true;
+        image.mapster('resize',width-diffW,0,200);
+        lastw=width;
+        lasth=height;
+        setTimeout(function() {
+            resizing=false;
+            resize();
+        },200);
+    } else {
+
+    }
+};
+$(window).bind('resize',resize);
 
 var tooltips = document.querySelectorAll('.modelElem > .modelElemTooltip');
 
